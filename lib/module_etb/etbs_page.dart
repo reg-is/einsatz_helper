288a6ea41ed2975cb280017f62117bf1bf2fe7cf
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-04-22 15:03:32 +0200

    add card for ETB

--- lib/module_etb/etbs_page.dart
+++ lib/module_etb/etbs_page.dart
@@ -18,12 +18,58 @@ class ETBsPage extends StatelessWidget {
               icon: Icon(Ionicons.search)),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'ETB',
-          style: TextStyle(fontSize: 60),
+      body: Center(
+          child: Card(
+        elevation: 8,
+        margin: EdgeInsets.all(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Chip(
+                  labelPadding: EdgeInsets.fromLTRB(2, -2, 2, -2),
+                  label: Text('42'),
+                  labelStyle: TextStyle(color: Colors.white),
+                  backgroundColor: Theme.of(context).unselectedWidgetColor,
+                  elevation: 1.0,
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  //labelStyle: ,
+                ),
+                Text(
+                  'Einsatz Petresweg I',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Chip(
+                  labelPadding: EdgeInsets.fromLTRB(2, -2, 2, -2),
+                  label: Text('Laufender Einsatz'),
+                  elevation: 1.0,
+                  //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  'Einsatzbeginn: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('261814Feb22')
+              ],
+            ),
+            Row(
+              children: [Text('Einsatzleitung: '), Text('Manuela Musterfrau')],
+            ),
+            Row(
+              children: [Text('ETB-Führung: '), Text('Max Mustermann')],
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
