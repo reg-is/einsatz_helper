f201125555f94ceb8db66261e60049c1d285dccb
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-04-22 20:24:25 +0200

    update Theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,29 +2,27 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class MyThemes {
   static final lightTheme = FlexThemeData.light(
-    scheme: FlexScheme.green,
-    usedColors: 1,
-    surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-    blendLevel: 20,
-    appBarStyle: FlexAppBarStyle.material,
-    appBarOpacity: 0.95,
-    appBarElevation: 1.5,
-    lightIsWhite: true,
-    tooltipsMatchBackground: true,
-    subThemesData: const FlexSubThemesData(
-      blendOnLevel: 20,
-      blendOnColors: false,
-      defaultRadius: 10.0,
-      inputDecoratorIsFilled: false,
-      inputDecoratorUnfocusedBorderIsColored: false,
-      fabRadius: 40.0,
-      bottomNavigationBarElevation: 8.0,
-    ),
-    visualDensity: FlexColorScheme.comfortablePlatformDensity,
-    useMaterial3: true,
-    // To use the playground font, add GoogleFonts package and uncomment
-    // fontFamily: GoogleFonts.notoSans().fontFamily,
-  );
+  scheme: FlexScheme.green,
+  usedColors: 1,
+  blendLevel: 5, // Higher -> Darker Scaffold background 
+  appBarStyle: FlexAppBarStyle.material,
+  appBarOpacity: 0.95,
+  appBarElevation: 1.5,
+  tooltipsMatchBackground: true,
+  subThemesData: const FlexSubThemesData(
+    blendOnLevel: 22,
+    blendOnColors: false,
+    defaultRadius: 10.0,
+    inputDecoratorIsFilled: false,
+    inputDecoratorUnfocusedBorderIsColored: false,
+    fabRadius: 40.0,
+    bottomNavigationBarElevation: 8.0,
+  ),
+  visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  useMaterial3: true,
+  // To use the playground font, add GoogleFonts package and uncomment
+  // fontFamily: GoogleFonts.notoSans().fontFamily,
+);
 
   static final darkTheme = FlexThemeData.dark(
     colors:
