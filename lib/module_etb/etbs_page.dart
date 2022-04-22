5a977f00c8e8784430419d8145962a1dcacfb8e8
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-04-22 17:54:18 +0200

    change ETB card

--- lib/module_etb/etbs_page.dart
+++ lib/module_etb/etbs_page.dart
@@ -22,52 +22,90 @@ class ETBsPage extends StatelessWidget {
           child: Card(
         elevation: 8,
         margin: EdgeInsets.all(8),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Chip(
-                  labelPadding: EdgeInsets.fromLTRB(2, -2, 2, -2),
-                  label: Text('42'),
-                  labelStyle: TextStyle(color: Colors.white),
-                  backgroundColor: Theme.of(context).unselectedWidgetColor,
-                  elevation: 1.0,
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  //labelStyle: ,
-                ),
-                Text(
-                  'Einsatz Petresweg I',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Chip(
-                  labelPadding: EdgeInsets.fromLTRB(2, -2, 2, -2),
-                  label: Text('Laufender Einsatz'),
-                  elevation: 1.0,
-                  //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  'Einsatzbeginn: ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text('261814Feb22')
-              ],
-            ),
-            Row(
-              children: [Text('Einsatzleitung: '), Text('Manuela Musterfrau')],
-            ),
-            Row(
-              children: [Text('ETB-Führung: '), Text('Max Mustermann')],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Chip(
+                    visualDensity: VisualDensity(horizontal: 0.0, vertical: -4),
+                    padding: EdgeInsets.all(0),
+                    label: Text('42'),
+                    labelStyle: TextStyle(color: Colors.white),
+                    backgroundColor: Theme.of(context).unselectedWidgetColor,
+                    elevation: 1.0,
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  Text(
+                    'Einsatz Petresweg 32',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Chip(
+                    visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                    padding: EdgeInsets.all(2),
+                    label: Text('Laufender Einsatz'),
+                    elevation: 1.0,
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Einsatzbeginn: ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text('261814Feb22')
+                ],
+              ),
+              Row(
+                children: [Text('Einsatzleitung: '), Text('Manuela Musterfrau')],
+              ),
+              Row(
+                children: [Text('ETB-Führung: '), Text('Max Mustermann')],
+              ),
+              Row(
+                children: [
+                  Chip(
+                    visualDensity: VisualDensity(horizontal: 0.0, vertical: -4),
+                    padding: EdgeInsets.all(2),
+
+                    label: Text('4 Einträge'),
+                    labelStyle: TextStyle(color: Colors.white),
+                    backgroundColor: Theme.of(context).unselectedWidgetColor,
+                    elevation: 1.0,
+                    //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  Chip(
+                    visualDensity: VisualDensity(horizontal: 0.0, vertical: -4),
+                    padding: EdgeInsets.all(2),
+
+                    label: Text('2 Anlagen'),
+                    labelStyle: TextStyle(color: Colors.white),
+                    backgroundColor: Theme.of(context).unselectedWidgetColor,
+                    elevation: 1.0,
+                    //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  Chip(
+                    visualDensity: VisualDensity(horizontal: 0.0, vertical: -4),
+                    padding: EdgeInsets.all(2),
+
+                    label: Text('4 Einträge'),
+                    labelStyle: TextStyle(color: Colors.white),
+                    backgroundColor: Theme.of(context).unselectedWidgetColor,
+                    elevation: 1.0,
+                    //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       )),
     );
