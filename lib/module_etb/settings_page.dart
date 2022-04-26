bab3894ecc26d8f1c60a433b69770bb3e4c9e665
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-04-26 09:13:16 +0200

    add settings page

--- lib/module_etb/settings_page.dart
+++ lib/module_etb/settings_page.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    bool extendAbbreviation = true;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: const Center(
-        child: Text(
-          'Settings',
-          style: TextStyle(fontSize: 60),
+        appBar: AppBar(
+          title: const Text('Settings'),
         ),
-      ),
-    );
+        body: ListView(
+          children: [
+            ListTile(
+              title: const Text('Kürzel ausschreiben'),
+              subtitle: const Text('Kürzel werden automatich ausgeschrieben.'),
+              trailing: Switch(
+                onChanged: (bool value) {
+                  extendAbbreviation = value;
+                },
+                value: extendAbbreviation,
+              ),
+            ),
+            const Divider(height: 1, thickness: 2),
+            ListTile(
+              title: const Text('Kürzeldatenbank'),
+              subtitle: const Text('Kürzel entfernen oder hinzufügen.'),
+              trailing: TextButton(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text('Verwalten'),
+                    Icon(Icons.arrow_forward)
+                  ],
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const Divider(height: 1, thickness: 2),
+            ListTile(
+              title: const Text('Organisation'),
+              //subtitle: const Text('Kürzel entfernen oder hinzufügen.'),
+            ),
+            const Divider(height: 1, thickness: 2),
+            ListTile(
+              title: const Text('Standard ETB-Führung'),
+              subtitle: const Text(
+                  'Dieser Name wir beim erstellen neuer ETBs genutzt.'),
+              trailing: Text('Max Mustermann'),
+            ),
+          ],
+        ));
   }
 }
