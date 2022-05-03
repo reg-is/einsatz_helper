91722979a6260fba5a0cd53f7c5d58a937175745
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-05-03 10:00:52 +0200

    add function to append an entry to a etb

--- lib/module_etb/data_box.dart
+++ lib/module_etb/data_box.dart
@@ -1,6 +1,21 @@
 import 'package:einsatz_helper/module_etb/model/etb_data.dart';
+import 'package:einsatz_helper/module_etb/model/etb_entry_data.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class DataBox {
   static Box<ETBData> getETBs() => Hive.box<ETBData>('etbBox');
+
+  static void appendEntry(dynamic etbKey, ETBEntryData entry) {
+    final etbBox = DataBox.getETBs();
+    ETBData? etb = etbBox.get(etbKey);
+    if (etb != null) {
+      List<ETBEntryData>? entries = etb.entries;
+      entries != null ? entries.add(entry) : entries = [entry];
+      etb.entries = entries;
+      etbBox.put(etbKey, etb);
+      print('Append done');
+    } else {
+      print('No ETB with key=$etbKey found');
+    }
+  }
 }
