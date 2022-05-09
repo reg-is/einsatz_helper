c010d19f484bd722b1c96f04cf83291aaf820a0e
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-05-09 15:02:32 +0200

    add form to create new templates

--- lib/module_etb/pages/add_template_page.dart
+++ lib/module_etb/pages/add_template_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+import '../data_box.dart';
+import '../model/template_data.dart';
 
 class AddTemplatePage extends StatefulWidget {
   const AddTemplatePage({Key? key}) : super(key: key);
@@ -9,11 +14,13 @@ class AddTemplatePage extends StatefulWidget {
 }
 
 class _AddTemplatePageState extends State<AddTemplatePage> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Eintrag anlegen'),
+        title: const Text('Vorlage anlegen'),
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
@@ -37,10 +44,100 @@ class _AddTemplatePageState extends State<AddTemplatePage> {
   }
 
   Widget buildNewTemplateForm(BuildContext context) {
-    return Text('Hello');
+    return FormBuilder(
+      key: _formKey,
+      child: Wrap(
+        spacing: 8,
+        runSpacing: 16,
+        children: [
+          FormBuilderTextField(
+            name: 'name',
+            decoration: const InputDecoration(
+              labelText: 'Name der Vorlage*',
+            ),
+            validator: FormBuilderValidators.required(),
+          ),
+          FormBuilderTextField(
+            name: 'counterpart',
+            decoration: const InputDecoration(
+              labelText: 'Gegenstelle',
+            ),
+          ),
+          FormBuilderTextField(
+            name: 'description',
+            decoration: const InputDecoration(
+              labelText: 'Darstellung des Ereignis',
+            ),
+            minLines: 4,
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+          ),
+          FormBuilderTextField(
+            name: 'comment',
+            decoration: const InputDecoration(
+              labelText: 'Bemerkung',
+            ),
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+          ),
+          Center(
+            child: Wrap(
+              runSpacing: 8,
+              spacing: 8,
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context, 'Cancel');
+                  },
+                  child: const Text('Verwerfen'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    bool result = onPressAccept();
+                    if (result) {
+                      Navigator.pop(context, 'Save');
+                    }
+                  },
+                  child: const Text('Vorlage speichern'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 8),
+        ],
+      ),
+    );
   }
 
+  // Is called when user accepts the inputted data.
   bool onPressAccept() {
-    return false;
+    _formKey.currentState!.save();
+    if (_formKey.currentState!.validate()) {
+      print(_formKey.currentState!.value);
+      addTemplate();
+      return true;
+    } else {
+      print("validation failed");
+      return false;
+    }
+  }
+
+  // Get user input data from form, create a new template and append it to the templateBox
+  Future addTemplate() async {
+    // Get user input data from form
+    final Map formInput = _formKey.currentState!.value;
+
+    // Create a new entry with input from form
+    final template = TemplateData()
+      //..id = DataBox.getNextEntryID(widget.etbKey)
+      ..name = formInput['name']
+      ..counterpart = formInput['counterpart']
+      ..description = formInput['description']
+      ..comment = formInput['comment']
+      ..creationTime = DateTime.now()
+      ..modificationTime = DateTime.now();
+
+    // Append new template in the templateBox
+    DataBox.getTemplates().add(template);
   }
 }
