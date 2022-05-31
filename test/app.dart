46f4eb907b5110067b2d4ba49feade4dcb1c7981
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-05-31 22:49:12 +0200

    add test app

---
+++ test/app.dart
@@ -0,0 +1,14 @@
+
+import 'package:einsatz_helper/main.dart' as app;
+import 'package:flutter/material.dart';
+
+//import 'package:flutter_driver/driver_extension.dart';
+
+
+void main() {
+  // Remove debug banner
+  WidgetsApp.debugAllowBannerOverride = false;
+  
+  // Run app
+  app.main();
+}
