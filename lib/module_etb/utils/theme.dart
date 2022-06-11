cb5037c767c4fe3f311f70b8a37e4c886296e74e
Régis Fayard <22980070+reg-is@users.noreply.github.com>
2022-06-12 01:19:43 +0200

    improve documentation

--- lib/module_etb/utils/theme.dart
+++ lib/module_etb/utils/theme.dart
@@ -1,33 +1,36 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
+/// Class containing the theme data for the app.
 class MyThemes {
+  /// Theme for light mode.
   static final lightTheme = FlexThemeData.light(
-  scheme: FlexScheme.green,
-  surfaceMode: FlexSurfaceMode.highBackgroundLowScaffold,
-  usedColors: 1,
-  blendLevel: 8, // Higher -> Darker Scaffold background 
-  lightIsWhite: true,
-  appBarStyle: FlexAppBarStyle.material,
-  appBarOpacity: 0.95,
-  appBarElevation: 1.5,
-  tooltipsMatchBackground: true,
-  subThemesData: const FlexSubThemesData(
-    cardElevation: 1,
-    blendOnLevel: 22,
-    blendOnColors: false,
-    defaultRadius: 5.0,
-    inputDecoratorIsFilled: false,
-    inputDecoratorUnfocusedBorderIsColored: false,
-    fabRadius: 40.0,
-    bottomNavigationBarElevation: 8.0,
-  ),
-  visualDensity: FlexColorScheme.comfortablePlatformDensity,
-  useMaterial3: true,
-  // To use the playground font, add GoogleFonts package and uncomment
-  // fontFamily: GoogleFonts.notoSans().fontFamily,
-);
+    scheme: FlexScheme.green,
+    surfaceMode: FlexSurfaceMode.highBackgroundLowScaffold,
+    usedColors: 1,
+    blendLevel: 8, // Higher -> Darker Scaffold background
+    lightIsWhite: true,
+    appBarStyle: FlexAppBarStyle.material,
+    appBarOpacity: 0.95,
+    appBarElevation: 1.5,
+    tooltipsMatchBackground: true,
+    subThemesData: const FlexSubThemesData(
+      cardElevation: 1,
+      blendOnLevel: 22,
+      blendOnColors: false,
+      defaultRadius: 5.0,
+      inputDecoratorIsFilled: false,
+      inputDecoratorUnfocusedBorderIsColored: false,
+      fabRadius: 40.0,
+      bottomNavigationBarElevation: 8.0,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    useMaterial3: true,
+    // To use the playground font, add GoogleFonts package and uncomment
+    // fontFamily: GoogleFonts.notoSans().fontFamily,
+  );
 
+  /// Theme for dark mode.
   static final darkTheme = FlexThemeData.dark(
     colors:
         FlexColor.schemes[FlexScheme.green]!.light.defaultError.toDark(0, true),
@@ -53,14 +56,14 @@ class MyThemes {
     // fontFamily: GoogleFonts.notoSans().fontFamily,
   );
 
-  // Custom border for Cards
+  /// Custom border for cards.
   static ShapeBorder myCardBorder(BuildContext context) {
     return RoundedRectangleBorder(
-            borderRadius:  BorderRadius.circular(5.0),
-            side: BorderSide(
-              color: Theme.of(context).shadowColor.withOpacity(0.1),
-              width: 1,
-            ),
-          );
-  } 
+      borderRadius: BorderRadius.circular(5.0),
+      side: BorderSide(
+        color: Theme.of(context).shadowColor.withOpacity(0.1),
+        width: 1,
+      ),
+    );
+  }
 }
